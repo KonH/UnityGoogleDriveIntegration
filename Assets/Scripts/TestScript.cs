b44b1572b49b5878f9b293a6ea0ce0753e631ff9
KonH <konh@yandex.ru>
2019-06-09 17:57:27 +0700

    Example usage

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+	public TestWorkSheet WorkSheet;
+	
+	public void Start() {
+		foreach ( var data in WorkSheet.dataArray ) {
+			Debug.Log($"A1: {data.A1}, B1: {data.B1}, C1: {data.C1}");
+		}
+	}
+}
